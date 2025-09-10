85b2958857b6171a212065254b40aa7cebd2fd05
kevin <murindakevinmulk@gmail.com>
2025-09-10 12:58:47 +0200

    doc: Setup the roadmap-service

---
+++ src/test/java/com/capstone/RoadmapServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapServiceApplicationTests {
+
+}

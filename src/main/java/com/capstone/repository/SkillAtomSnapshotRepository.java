f6ea22ecd24f89d52d783387fa464860536102e0
kevin <murindakevinmulk@gmail.com>
2025-09-12 17:23:32 +0200

    feat: Add additional derived queries

--- src/main/java/com/capstone/repository/SkillAtomSnapshotRepository.java
+++ src/main/java/com/capstone/repository/SkillAtomSnapshotRepository.java
@@ -5,8 +5,11 @@ import com.capstone.model.SkillAtomSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SkillAtomSnapshotRepository extends JpaRepository<SkillAtomSnapshot, UUID> {
+    Optional<SkillAtomSnapshot> findBySkillAtomId(UUID skillAtomId);
+    boolean existsBySkillAtomId(UUID skillAtomId);
 }

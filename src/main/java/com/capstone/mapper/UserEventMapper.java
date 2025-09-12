89d94df7cee40eac783ad1e575d30e0ee4591217
kevin <murindakevinmulk@gmail.com>
2025-09-12 09:47:49 +0200

    feat: Add mapping interface between kafka event and UserSnapshot

---
+++ src/main/java/com/capstone/mapper/UserEventMapper.java
@@ -0,0 +1,23 @@
+package com.capstone.mapper;
+
+import com.capstone.model.UserSnapshot;
+import org.common.event.ProduceUserEvent;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface UserEventMapper {
+
+    @Mapping(source = "versapathUserId", target = "userId")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    UserSnapshot toUserSnapshot(ProduceUserEvent event);
+
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateUserSnapshot(ProduceUserEvent event, @MappingTarget UserSnapshot userSnapshot);
+}

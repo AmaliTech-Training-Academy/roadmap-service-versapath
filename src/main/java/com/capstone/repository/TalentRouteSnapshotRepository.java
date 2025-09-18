8eefc57ad1bd400043488d2b7d26da0f43442139
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-18 09:42:42 +0200

    feat: create leaner roadmap

--- src/main/java/com/capstone/repository/TalentRouteSnapshotRepository.java
+++ src/main/java/com/capstone/repository/TalentRouteSnapshotRepository.java
@@ -4,8 +4,10 @@ import com.capstone.model.TalentRouteSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TalentRouteSnapshotRepository extends JpaRepository<TalentRouteSnapshot, UUID> {
+    Optional<TalentRouteSnapshot> findByTalentRouteId(UUID talentRouteId);
 }
--- src/main/java/com/capstone/service/impl/LearnerRoadmapServiceImpl.java
+++ src/main/java/com/capstone/service/impl/LearnerRoadmapServiceImpl.java
@@ -1,6 +1,13 @@
 package com.capstone.service.impl;
 
+import com.capstone.exception.TalentRouteNotFoundException;
+import com.capstone.exception.UserNotFoundException;
+import com.capstone.model.EnrollmentStatus;
+import com.capstone.model.LearnerRoadmap;
+import com.capstone.model.TalentRouteSnapshot;
 import com.capstone.repository.LearnerRoadmapRepository;
+import com.capstone.repository.TalentRouteSnapshotRepository;
+import com.capstone.repository.UserSnapshotRepository;
 import com.capstone.service.LearnerRoadmapService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,9 +18,31 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class LearnerRoadmapServiceImpl implements LearnerRoadmapService {
     private final LearnerRoadmapRepository learnerRoadmapRepository;
+    private final TalentRouteSnapshotRepository talentRouteSnapshotRepository;
+    private final UserSnapshotRepository userSnapshotRepository;
     @Override
     public String assignLearnerToTalentRoute(UUID learnerId, UUID talentRouteId) {
-        //TODO: assign learner to talent route
+
+        LearnerRoadmap learnerRoadmap = createLearnerRoadmap(learnerId, talentRouteId);
+
+        learnerRoadmapRepository.save(learnerRoadmap);
         return null;
+    }
+
+    private LearnerRoadmap createLearnerRoadmap(UUID learnerId, UUID talentRouteId){
+        TalentRouteSnapshot talentRoute = talentRouteSnapshotRepository.findByTalentRouteId(talentRouteId)
+                .orElseThrow( () -> new TalentRouteNotFoundException("A talent route provided doesn't exist")
+                );
+
+        if(userSnapshotRepository.findByUserId(learnerId).isEmpty()){
+            throw new UserNotFoundException("A leaner provided doesn't exist");
+        }
+
+        return LearnerRoadmap.builder()
+                .talentRoute(talentRoute)
+                .userId(learnerId)
+                .enrollmentStatus(EnrollmentStatus.ACTIVE)
+                .overallProgressPercentage(0)
+                .build();
     }
 }

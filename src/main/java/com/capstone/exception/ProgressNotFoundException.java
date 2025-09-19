864c2bf89c5351d53715744d98073968d43e5a0c
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-19 21:13:51 +0200

    feat: update learner atom progress status to IN_PROGRESS

---
+++ src/main/java/com/capstone/exception/ProgressNotFoundException.java
@@ -0,0 +1,7 @@
+package com.capstone.exception;
+
+public class ProgressNotFoundException extends RuntimeException {
+    public ProgressNotFoundException(String message) {
+        super(message);
+    }
+}

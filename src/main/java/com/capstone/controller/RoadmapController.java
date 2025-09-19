f8cad199e25da7ecb3c162f3338f647b88562932
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-19 21:19:04 +0200

    ref: add trackId field to be more specific when selecting atom

--- src/main/java/com/capstone/controller/RoadmapController.java
+++ src/main/java/com/capstone/controller/RoadmapController.java
@@ -1,6 +1,8 @@
 package com.capstone.controller;
 
 import com.capstone.dto.roadmap.RoadmapRequestDto;
+import com.capstone.dto.roadmap.StartAtomProgressRequestDto;
+import com.capstone.service.LearnerProgressService;
 import com.capstone.service.LearnerRoadmapService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,10 +17,18 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Roadmap Controller", description = "Manage all roadmap's api")
 public class RoadmapController {
     private final LearnerRoadmapService learnerRoadmapService;
+    private final LearnerProgressService learnerProgressService;
     @PostMapping()
     @Operation(summary = "Assign track", description = "This end point allows a learner to select talent route")
     public ResponseEntity<String> assignRouteToLearner(@RequestBody RoadmapRequestDto roadmapRequestDto) {
         learnerRoadmapService.assignLearnerToTalentRoute(roadmapRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body("response");
+    }
+
+    @PostMapping("/start-progress")
+    @Operation(summary = "Start progress", description = "This end point gets a learner to start tracking progress")
+    public ResponseEntity<String> startTrackingProgress(@RequestBody StartAtomProgressRequestDto dto) {
+        learnerProgressService.startAtomProgress(dto);
         return ResponseEntity.status(HttpStatus.OK).body("response");
     }
 

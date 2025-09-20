783b7494d9fc0cbc4de463c4d11311b28476cff0
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-20 17:22:44 +0200

    feat: add non-existing growth tracks to learner roadmap

--- src/main/java/com/capstone/service/LearnerProgressService.java
+++ src/main/java/com/capstone/service/LearnerProgressService.java
@@ -1,8 +1,12 @@
 package com.capstone.service;
 
 import com.capstone.dto.roadmap.AtomProgressRequestDto;
+import com.capstone.dto.roadmap.RecalculateProgressRequestDto;
+
+import java.util.UUID;
 
 public interface LearnerProgressService {
     String startAtomProgress(AtomProgressRequestDto atomProgressRequestDto);
     String completeAtomProgress(AtomProgressRequestDto atomProgressRequestDto);
+    String recalculateAndUpdateLearnerRoadmap(RecalculateProgressRequestDto dto);
 }

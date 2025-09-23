ffdbbdf37364435a8b5c51976b6e1923839ddc5f
kevin <murindakevinmulk@gmail.com>
2025-09-23 12:31:19 +0200

    feat: Extend LearnerRoadmapService with learner view methods

--- src/main/java/com/capstone/service/LearnerRoadmapService.java
+++ src/main/java/com/capstone/service/LearnerRoadmapService.java
@@ -1,7 +1,20 @@
 package com.capstone.service;
 
+import com.capstone.dto.response.LearnerAtomProgressDto;
+import com.capstone.dto.response.LearnerCapsuleProgressDto;
+import com.capstone.dto.response.LearnerRoadmapWithProgressDto;
+import com.capstone.dto.response.LearnerTrackProgressDto;
 import com.capstone.dto.roadmap.RoadmapRequestDto;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface LearnerRoadmapService {
     void assignLearnerToTalentRoute(RoadmapRequestDto roadmapRequestDto);
+
+    Optional<LearnerRoadmapWithProgressDto> getLearnerRoadmap(UUID learnerId);
+    List<LearnerTrackProgressDto> getLearnerTracks(UUID learnerId);
+    List<LearnerCapsuleProgressDto> getTrackCapsules(UUID learnerId, UUID trackId);
+    List<LearnerAtomProgressDto> getCapsuleAtoms(UUID learnerId, UUID capsuleId);
 }

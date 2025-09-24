2993fb973fa228eca0a28db1cdde34baaaa931ef
kevin <murindakevinmulk@gmail.com>
2025-09-24 13:02:20 +0200

    feat: add exception method

--- src/main/java/com/capstone/exception/TalentRouteNotFoundException.java
+++ src/main/java/com/capstone/exception/TalentRouteNotFoundException.java
@@ -1,7 +1,12 @@
 package com.capstone.exception;
 
+import java.util.UUID;
+
 public class TalentRouteNotFoundException extends RuntimeException {
     public TalentRouteNotFoundException(String message) {
         super(message);
+    }
+    public TalentRouteNotFoundException(UUID routeId) {
+        super("Talent route not found with ID: " + routeId);
     }
 }

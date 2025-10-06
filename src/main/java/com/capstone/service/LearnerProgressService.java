5291dd59a41d93f7bcbc1b24a371d91ec6634247
kevin <murindakevinmulk@gmail.com>
2025-10-06 11:34:17 +0200

    feat: add bulk progress recalculation method to LearnerProgressService

--- src/main/java/com/capstone/service/LearnerProgressService.java
+++ src/main/java/com/capstone/service/LearnerProgressService.java
@@ -3,8 +3,12 @@ package com.capstone.service;
 import com.capstone.dto.request.AtomProgressRequestDto;
 import com.capstone.dto.request.RecalculateProgressRequestDto;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface LearnerProgressService {
     void startAtomProgress(AtomProgressRequestDto atomProgressRequestDto);
     void completeAtomProgress(AtomProgressRequestDto atomProgressRequestDto);
     void recalculateAndUpdateLearnerRoadmap(RecalculateProgressRequestDto dto);
+    void bulkRecalculateProgress(List<UUID> learnerIds, List<UUID> talentRouteIds);
 }
